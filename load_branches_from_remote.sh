31833d1d1823cd76c459a007d4c40ed17dc242ef
Ehud <ehud@nymhealth.com>
2021-02-08 17:27:56 +0200

    random changes found on old laptop

--- load_branches_from_remote.sh
+++ load_branches_from_remote.sh
@@ -6,15 +6,15 @@
 #
 # Example: load_branches_from_remote.sh origin
 
-REMOTE=$1
+REMOTE=stingray
 echo "Loading all branches from the remote '$REMOTE' (all local branches are deleted)"
 # Checking out orphan commit so it 's possible to delete current branch
 # Create non-remote-tracking branches from selected remote
 for REMOTE_BRANCH in $(git branch -r|grep $REMOTE/); do
     BRANCH=${REMOTE_BRANCH/$REMOTE\//$REMOTE\_}
     echo "Branch name: '$BRANCH', Remote: '$REMOTE', Remote Branch: '$REMOTE_BRANCH'"
-    git branch -q $BRANCH $REMOTE_BRANCH
+    git branch $BRANCH $REMOTE_BRANCH
     git branch --unset-upstream $BRANCH
 done
-git checkout -f $REMOTE"_develop"
+# git checkout -f $REMOTE"_develop"
 
